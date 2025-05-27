071d013a762a2a11f340c52035c25f81e97c1f7b
Shalman212 <salmanshadhin7@gmail.com>
2025-05-27 13:38:29 +0600

    Updated components and added BestSeller & ProductItem

--- Tashion/src/context/ShopContext.jsx
+++ Tashion/src/context/ShopContext.jsx
@@ -1,14 +1,18 @@
-import { createContext } from "react";
-import { products } from "../assets/assets.js";
+import React, { createContext } from "react";
+import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
 
-const ShopContextProvider = (props) => {
-    const currency = '$';
+export const ShopContextProvider = (props) => {
+    const currency = "$";
     const delivery_fee = 10;
+
     const value = {
-        products, currency, delivery_fee
+        products,
+        currency,
+        delivery_fee,
     };
+
     return (
         <ShopContext.Provider value={value}>
             {props.children}
