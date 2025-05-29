732ef6dad50a8cff43be979e1c4db25c44f48300
Shalman212 <salmanshadhin7@gmail.com>
2025-05-29 22:38:28 +0600

    Added SearchBar component and updated Navbar, App, ShopContext, and Collection

--- Tashion/src/pages/Collection.jsx
+++ Tashion/src/pages/Collection.jsx
@@ -8,15 +8,16 @@ const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
-  const [category, setCategory]= useState([]);
-  const [subCategory, setSubCategory]= useState([]);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState("relevant");
 
-  const toggleCategory =(e)=>{
-    if(category.includes(e.target.value)){
-      setCategory(prev=> prev.filter(item=> item!==e.target.value))
+  const toggleCategory = (e) => {
+    if (category.includes(e.target.value)) {
+      setCategory(prev => prev.filter(item => item !== e.target.value))
     }
-    else{
-      setCategory(prev=> [...prev,e.target.value]);
+    else {
+      setCategory(prev => [...prev, e.target.value]);
     }
   }
   const toggleSubCategory = (e) => {
@@ -28,33 +29,47 @@ const Collection = () => {
     }
   }
 
-  const applyFilter =()=>{
-    let productsCopy=products.slice();
-    if(category.length>0){
-      productsCopy=productsCopy.filter(item=> category.includes(item.category));
+  const applyFilter = () => {
+    let productsCopy = products.slice();
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter(item => category.includes(item.category));
     }
     if (subCategory.length > 0) {
       productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory));
     }
-    
+
     setFilterProducts(productsCopy);
   }
-  
-  useEffect(()=> {
-    setFilterProducts(products);
-  },[])
+  const sortProduct = () => {
+    let fpCopy = filterProducts.slice();
+    switch (sortType) {
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a, b) => (a.price - b.price)));
+        break;
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a, b) => (b.price - a.price)));
+        break;
+      default:
+        applyFilter();
+        break;
+    }
+  }
 
-  useEffect(()=>{
+  useEffect(() => {
     applyFilter();
-  },[category,subCategory])
+  }, [category, subCategory])
+
+  useEffect(() => {
+    sortProduct();
+  }, [sortType]);
   
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       {/* {Filter Options} */}
       <div className='min-w-60'>
-        <p onClick={()=>setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>
+        <p onClick={() => setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>
           FILTERS
-          <img src={assets.dropdown_icon} className={`h-3 sm:hidden ${showFilter? 'rotate-90' : ''}`} alt="" />
+          <img src={assets.dropdown_icon} className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} alt="" />
         </p>
         {/* Category Filter */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
@@ -100,10 +115,10 @@ const Collection = () => {
       {/* Right Side  */}
       <div className='flex-1 '>
         <div className='flex justify-around text-base sm:text-2xl mb-4'>
-          <Title text1={'ALL'} text2={'COLLECTION'}/>
+          <Title text1={'ALL'} text2={'COLLECTION'} />
           {/* Product Sort  */}
-          <select className='border-2 border-gray-300 text-sm px-2'>
-            <option value="relevent">Sort By: Relevent</option>
+          <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
+            <option value="relevant">Sort By: Relevant</option>
             <option value="low-high">Sort By: Low to High</option>
             <option value="high-low">Sort By: High to Low</option>
           </select>
@@ -111,8 +126,8 @@ const Collection = () => {
         {/* map products  */}
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
           {
-            filterProducts.map((item,index)=> (
-              <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image}/>
+            filterProducts.map((item, index) => (
+              <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
             ))
           }
         </div>
